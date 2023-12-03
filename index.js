0537f12bb58c74c59498374f325fa76af7db18c9
ron60bro <ronalshuvamboruah@gmail.com>
2023-12-03 22:43:05 +0530

    check

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ console.log("Heavy");
 
 console.log("I'm working on a branch which is from a branch not the main");;
 
+function B(){
+    return 1+1;
+}
 function A(){
     console.log("hhh")
 }
+B();
 A();
 console.log("I'm happy")
