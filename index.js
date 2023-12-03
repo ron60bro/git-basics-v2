be8d9a28f993b8779d07b84cfe576b098f7b9972
ron60bro <ronalshuvamboruah@gmail.com>
2023-12-03 22:33:56 +0530

    qwert

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 console.log("Hello "),
 console.log("Heavy");
+console.log("Conflict")
+console.log("I'm working on a branch which is from a branch not the main");
 
-console.log("I'm working on a branch which is from a branch not the main");;
-
+console.log("check again if conflict")
 function A(){
     console.log("hhh")
 }
